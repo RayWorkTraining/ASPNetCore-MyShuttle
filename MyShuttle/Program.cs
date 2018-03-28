fb1b9dea41e8ab93524f6e8c20329210ac7e9b6f
RayWorkTraining <rtang@azdes.gov>
2018-03-28 20:39:25 +0000

    Module3Updates

--- MyShuttle/Program.cs
+++ MyShuttle/Program.cs
@@ -7,14 +7,32 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MyShuttle.Data;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace MyShuttle
+namespace MyShuttle.Web
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    MyShuttleDataInitializer.InitializeDatabaseAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- MyShuttle/Startup.cs
+++ MyShuttle/Startup.cs
@@ -6,29 +6,60 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using MyShuttle.Web.AppBuilderExtensions;
+using Microsoft.Extensions.Configuration;
+using MyShuttle.Model;
+using Microsoft.AspNetCore.Identity;
+using MyShuttle.Data;
 
 namespace MyShuttle
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("config.json", optional: true).SetBasePath(env.ContentRootPath).Build();
+            Configuration = config;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureDataContext(Configuration);
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<MyShuttleContext>().AddDefaultTokenProviders();
+
+            services.ConfigureDependencies();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.ConfigureRoutes();
         }
+
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+
+        //    app.UseMvc(routes =>
+        //    {
+        //        routes.MapRoute(name: "Default", template: "{controller}/{action}/{id?}", defaults: new { controller = "Home", action = "Index" });
+        //    });
+
+        //    //app.Run(async (context) =>
+        //    //{
+        //    //    await context.Response.WriteAsync("Hello World!");
+        //    //});
+        //}
     }
 }
